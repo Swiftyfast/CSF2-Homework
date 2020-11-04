fa88b397d8f986b4211d0d908ecce3ea029cf252
John Swift <jdavidswift@gmail.com>
2020-11-04 09:06:56 -0600

    Did both types of object creation in the Program.cs

--- TesterProgram/Program.cs
+++ TesterProgram/Program.cs
@@ -11,23 +11,55 @@ namespace TesterProgram
     {
         static void Main(string[] args)
         {
+            //*******
+            Student mumford = new Student("Marcus", "Mumford", "1900", 3.2f);
+
             Student ferdinand = new Student();
             ferdinand.LastName = "Ferdinand";
             ferdinand.FirstName = "Franz";
+            ferdinand.Gpa = 3.9f;
+            ferdinand.Id = "17";
 
             Console.WriteLine(ferdinand);
+            Console.WriteLine(mumford);
 
+            //******
             Vehicle falcon = new Vehicle("Corellian Corvette", "YT-1300", 5, 30000);
 
+            Vehicle enterprise = new Vehicle();
+            enterprise.Make = "Federation Starship";
+            enterprise.Model = "Galaxy Class";
+            enterprise.Year = 2300;
+            enterprise.Weight = 1000000;
+
+            Console.WriteLine(enterprise);
             Console.WriteLine(falcon);
+
+            //*******
 
             Login myActualLogin = new Login("myName", "12345");
 
+            Login secondLogin = new Login();
+            secondLogin.Username = "madeUpName";
+            secondLogin.Password = "020202";
+
             Console.WriteLine(myActualLogin);
+            Console.WriteLine(secondLogin);
+
+            //********
 
             ContactInfo myInfo = new ContactInfo("3180 Main Street", "Kansas City", "KS", "45825", "913-555-5885", "email@email.com");
 
+            ContactInfo newAddress = new ContactInfo();
+            newAddress.StreetAddress = "100 S. Baker Street";
+            newAddress.City = "London";
+            newAddress.State = "MO";
+            newAddress.Zip = "10000";
+            newAddress.Phone = "816-555-4000";
+            newAddress.Email = "notADoctor@email.com";
+
             Console.WriteLine(myInfo);
+            Console.WriteLine(newAddress);
         }
     }
 }
